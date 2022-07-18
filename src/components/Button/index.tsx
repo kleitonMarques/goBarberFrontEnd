6876dc8685ac5d6ac2867deed61fbbb027b6ece7
Kleiton Marques <kleitonm.marques@gmail.com>
2022-07-18 20:00:26 -0300

    insulatting components

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,9 +4,9 @@ import { Container } from './styles';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button: React.FC<ButtonProps> = () => (
-  <Container>
-    <button type="button">teste</button>
+const Button: React.FC<ButtonProps> = ({ children, ...rest }) => (
+  <Container type="button" {...rest}>
+    {children}
   </Container>
 );
 
